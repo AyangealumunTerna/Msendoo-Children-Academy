564add6ce22916713f9a7a49cdf115f5ce2b35ba
AyangealumunTerna <ayangealumunternasabastine@gmail.com>
2025-11-06 14:51:57 +0100

    Fix Railway build: add railway.json and start scrip

--- Backend/server.js
+++ Backend/server.js
@@ -104,7 +104,7 @@ app.get('/dashboard', verifyToken, (req, res) => {
 });
 
 // Start server
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
 // app.listen(5000, () => console.log('🚀 Server running on port 5000'));
