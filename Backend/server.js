feb645bfc8f8b76802f5f50d86f28f10b52b86ee
AyangealumunTerna <ayangealumunternasabastine@gmail.com>
2025-11-07 17:55:34 +0100

    Move backend start script to root for Railway

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 // Database connection
-const db = mysql.createConnection(process.env.DATABASE_URL);
+const db = mysql.createConnection(process.env.MYSQL_URL);
 
 db.connect(err => {
   if (err) {
